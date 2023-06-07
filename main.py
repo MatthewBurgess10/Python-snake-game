d738d6f5b89c1352b11ef56ba05a1c69408f3b5c
Matthew Burgess <matthew456burgess@btinternet.com>
2023-06-07 19:59:41 +0100

    nearly finished converting to oop

--- main.py
+++ main.py
@@ -1,12 +1,40 @@
 import pygame
 from pygame.locals import * 
+import sys
+
+class snake:
+    def __init__(self, main_screen):   #self allows u to access stuff in a defined class
+        self.main_screen = main_screen
+        self.large_block = pygame.image.load("snakeblock.png").convert()  #loads the image
+        self.block = pygame.transform.scale(self.large_block, (50, 50)) #shrinks the image
+        self.block_x = 100
+        self.block_y = 100
+    def draw(self, surface):
+        surface.blit(self.block, (self.block_x, self.block_y))
+class game:
+    def __init__(self):
+        pygame.init()
+        self.surface = pygame.display.set_mode((1000, 500))
+        self.surface.fill((12, 17, 17))
+    
+    def run(self):
+        self.main_screen.blit(self.block, (self.block_x, self.block_y)) #draws the image at these coords
+        
+        
+        
+def draw_block(surface, self):
+    surface.fill((12, 17, 17))
+    surface.blit(self.block, (self.block_x, self.block_y))
+    pygame.display.flip()
+    
 
 if __name__ == "__main__":
-    pygame.init()
+    Game = game()
+    game.run()
     
-    surface = pygame.display.set_mode((500, 500))
-    surface.fill((255,255,255))
-    pygame.display.flip()
+
+
+    pygame.display.flip()  #makes changes visible to the user.
     
     
 run = True
@@ -14,13 +42,28 @@ run = True
 while run == True:
     for event in pygame.event.get():
         if event.type == KEYDOWN:
-            pass
-        elif event.ype == QUIT:
+            if event.key == K_ESCAPE:
+                run = False
+                
+            if event.key == K_UP:
+                block_y = block_y - 10
+                draw_block()
+            if event.key == K_DOWN:
+                block_y = block_y + 10
+                draw_block()
+            if event.key == K_RIGHT:
+                block_x = block_x + 10
+                draw_block()
+            if event.key == K_LEFT:
+                block_x = block_x - 10 
+                draw_block()
+                
+        elif event.type == QUIT:
             run = False
     
     
     
-#video finished at 11.30 
+#video finished at 
  
     
     
