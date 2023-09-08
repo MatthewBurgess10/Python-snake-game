7b489c568d228855a4789d4176f9c592c8c9d2bb
Matthew Burgess <matthew456burgess@btinternet.com>
2023-09-08 20:27:05 +0100

    FIXED BUG AND WORKING WELL

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ class snake:
         self.block_x = [SIZE]*length
         self.block_y = [SIZE]*length
         self.direction = 'down'
+           #initializes an empty set
 
-           
     def draw(self):
         self.parent_screen.fill((53, 94, 59))
         for i in range(self.length):
@@ -67,7 +67,8 @@ class snake:
             self.block_x[i] = self.block_x[i - 1]
             self.block_y[i] = self.block_y[i - 1]
         self.draw()
-
+        
+        
 class Game:
     def __init__(self):    #this is the game initialization function
         pygame.init()
@@ -85,7 +86,30 @@ class Game:
         score = font.render(f"score: {self.snake.length-1}", True, (250, 250, 250))
         self.surface.blit(score,(900, 10))
         
-    
+    def play(self):
+        
+        self.snake.walk()
+        self.display_score()
+        self.apple.apple_draw()
+        
+        #check for collisions with the apple
+        if self.is_collision(self.snake.block_x[0], self.snake.block_y[0], self.apple.b_x, self.apple.b_y):
+            self.apple.apple_move()
+            self.snake.inc_length()
+        # Check for collisions with itself   
+        for i in range(2, self.snake.length):
+            if self.is_collision(self.snake.block_x[0], self.snake.block_y[0], self.snake.block_x[i], self.snake.block_x[i]):
+                raise "game over"   
+        # Check for collisions with walls 
+        if (
+            self.snake.block_x[0] < 0
+            or self.snake.block_x[0] >= 1000
+            or self.snake.block_y[0] < 0
+            or self.snake.block_y[0] >= 1000                 
+        ):
+            print("hit a wall")
+            self.game_over = True
+        
     def is_collision(self, x1, y1, x2, y2):
         x1_min, x1_max = x1, x1 + SIZE
         y1_min, y1_max = y1, y1 + SIZE
@@ -107,48 +131,44 @@ class Game:
    
     def run(self):
         self.running = True
-        
+        pause = False
         while self.running == True:
             for event in pygame.event.get():
                 if event.type == KEYDOWN:
                     if event.key == K_ESCAPE:
                         self.running = False
-                    if event.key == K_UP:
-                        self.snake.direction = 'up'
-                    if event.key == K_DOWN:
-                        self.snake.direction = 'down'
-                    if event.key == K_LEFT:
-                        self.snake.direction = 'left'
-                    if event.key == K_RIGHT:
-                        self.snake.direction = 'right'
-                elif event.type == QUIT:
+                    if not pause:
+                        
+                        if event.key == K_UP:
+                            self.snake.direction = 'up'
+                        if event.key == K_DOWN:
+                            self.snake.direction = 'down'
+                        if event.key == K_LEFT:
+                            self.snake.direction = 'left'
+                        if event.key == K_RIGHT:
+                            self.snake.direction = 'right'
+                    elif event.type == QUIT:
                     
-                    self.running = False
+                        self.running = False
+             
                 
-            self.snake.walk()
-            self.display_score()
-            self.apple.apple_draw()
+            try:
+                if not pause:
+                    self.play()
+            except Exception as e:
+                self.gameover()
+                pause = True
+            
+                
 
-            #check for collisions with the apple
-            if self.is_collision(self.snake.block_x[0], self.snake.block_y[0], self.apple.b_x, self.apple.b_y):
-                self.apple.apple_move()
-                self.snake.inc_length()
+            
                 
             #check for collisions with itself
-            for i in range(self.snake.length-1, self.snake.length):    #finishing the game when it gets to four???
-                if self.is_collision(self.snake.block_x[0], self.snake.block_y[0], self.snake.block_x[i], self.snake.block_x[i]):
-                    print("collision w itself")
-                    self.gameover()
+            
+            
 
-                # Check for collisions with walls 
-            if (
-                self.snake.block_x[0] < 0
-                or self.snake.block_x[0] >= 1000
-                or self.snake.block_y[0] < 0
-                or self.snake.block_y[0] >= 1000                 
-            ):
-                print("hit a wall")
-                self.game_over = True
+                  
+            
                 
             
             
